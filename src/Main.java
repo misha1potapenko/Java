36a51232a9d1e0be5bb44fc53041f883d397fae1
misha1potapenko <misha1potapenko@yandex.ru>
2022-11-22 20:23:54 +0300

    added Tn

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
+// 1. Вычислить n-ое треугольного число(сумма чисел от 1 до n),
+// n! (произведение чисел от 1 до n)
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner iScanner = new Scanner(System.in);
+        System.out.print("Введите число n = ");
+        int n = iScanner.nextInt();
+        int b = n + 1;
+        int k = (n * b) / 2;
+        System.out.print("Tn = ");
+        System.out.print(k);
+        iScanner.close();
     }
 }
