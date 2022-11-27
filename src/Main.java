56b3cba5d34ba0d4138ca77e8126a06a52b71507
misha1potapenko <misha1potapenko@yandex.ru>
2022-11-27 17:32:35 +0300

    add first task

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+//В файле содержится строка с исходными данными в такой форме:
+//        {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+//        Требуется на её основе построить и вывести на экран новую строку, в форме SQL запроса:
+//        SELECT * FROM students WHERE name = "Ivanov" AND country = "Russia" AND city = "Moscow";
+//
+//        Для разбора строки используйте String.split.
+//        Сформируйте новую строку, используя StringBuilder. Значения null не включаются в запрос.
 
 public class Main {
     public static void main(String[] args) {
