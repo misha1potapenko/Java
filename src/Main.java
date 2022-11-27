89be98818b2cea02906e0d388a223e7fa747a925
misha1potapenko <misha1potapenko@yandex.ru>
2022-11-27 18:30:12 +0300

    add split

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,30 @@
 //        Для разбора строки используйте String.split.
 //        Сформируйте новую строку, используя StringBuilder. Значения null не включаются в запрос.
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            File file = new File("C:\\Users\\misha\\IdeaProjects\\secondSem\\semSecondString\\src\\text.txt");
+            //создаем объект FileReader для объекта File
+            FileReader fr = new FileReader(file);
+            //создаем BufferedReader с существующего FileReader для построчного считывания
+            BufferedReader reader = new BufferedReader(fr);
+            // считаем сначала первую строку
+            String line = reader.readLine();
+            String[] words = line.split(" ");
+            System.out.println(line);
+            for (String word : words) {
+                System.out.println(word);
+            }
+//            while (line != null) {
+//                System.out.println(line);
+//                // считываем остальные строки в цикле
+//                line = reader.readLine();
+//            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
