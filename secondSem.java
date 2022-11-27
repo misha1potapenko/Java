f215f000d1b5ae60b3a577c1a6522c8f9dc980f0
misha1potapenko <misha1potapenko@yandex.ru>
2022-11-27 17:06:51 +0300

    added first task

---
+++ secondSem.java
@@ -0,0 +1,12 @@
+//В файле содержится строка с исходными данными в такой форме:
+//        {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+//        Требуется на её основе построить и вывести на экран новую строку, в форме SQL запроса:
+//        SELECT * FROM students WHERE name = "Ivanov" AND country = "Russia" AND city = "Moscow";
+//
+//        Для разбора строки используйте String.split.
+//        Сформируйте новую строку, используя StringBuilder. Значения null не включаются в запрос.
+public class secondSem {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
